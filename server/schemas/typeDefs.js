7fb5815c1145076fe50b7b9518f084f98557c585
saevans86 <sam_evans1986@hotmail.com>
2023-11-13 11:40:46 -0700

    still working on error

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,26 +1,34 @@
 const typeDefs = `
-type User {
-      _id: ID
+
+type Query {
+  me: User
+}
+  type User {
+    _id: ID
     username: String
     email: String
     password: String
-   
-    savedBooks(_id: String): [bookSchema]
-
-}
-Type Book {
-
-    bookId: ID
-  authors: String
-  description: String
-  title: String
-  image: String
-  link: string
-}
-      type Auth {
+    bookCount: [savedBooks]!
+  }
+  type Book {
+    authors: [authors] String
+    description: String!
+    bookId: ID!
+    image: String
+    title: String!
+  }
+  type Auth {
     token: ID!
     user: User
   }
-}
+    type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(authors: String[authors], description: String!, bookId: String!, image: String, link: String!, title: String! ): Book
+    removeBook(bookId: String!) User
+
+    }
+
+
 `;
 module.exports = typeDefs;
